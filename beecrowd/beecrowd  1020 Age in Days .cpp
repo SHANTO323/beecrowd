73c8703b3e130e205733c5b5ae96724501400551
lifeisshame <94046524+SHANTO323@users.noreply.github.com>
2022-07-13 12:37:22 +0600

    Add files via upload

---
+++ beecrowd/beecrowd  1020 Age in Days .cpp
@@ -0,0 +1,16 @@
+#includeiostream
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin  N;
+
+	cout  N  365   ano(s)  endl;
+	N %= 365;
+	cout  N  30   mes(es)  endl;
+	N %= 30;
+	cout  N   dia(s)  endl;
+	return 0;
+}
